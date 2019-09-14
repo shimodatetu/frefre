9f39c3b360ee797f77e8b785b70c9b2b5bd75308
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2019-09-14 10:29:03 +0900

    asd

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(data)
     @data = data
-    mail to: "frefre@info.net", subject: "FreFreへのお問い合わせ"
+    mail to: "info@frefre.net", subject: "FreFreへのお問い合わせ"
   end
 end
