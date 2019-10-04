426587c0dd48a6d61f2f3e74d08449beae4f16ef
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2019-10-04 21:31:55 +0900

    asd

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -21,13 +21,15 @@ class RoomChannel < ApplicationCable::Channel
       post.save
       info = Userinfo.new()
       info_page = (post.id_ingroup - 1) / 10 + 1
+
       info.title_en = 'There is a reply to "'+ group.title_en + '"!'
-      info.title_jp = '「'+group.title_en+'」に投稿がありました！'
+      info.title_jp = '「'+group.title_jp+'」に投稿がありました！'
 
       info.message_en = 'There is a reply to "'+ group.title_en + '".
       Please click on the url below and check the thread.
       https://www.frefreforum.com/'+group.id.to_s+'/'+info_page.to_s
-      info.message_jp = '「'+group.title_en+'」に投稿がありました。
+
+      info.message_jp = '「'+group.title_jp'」に投稿がありました。
       下記のURLをクリックしてスレッドを確認してください。
       https://www.frefreforum.com/'+group.id.to_s+'/'+info_page.to_s
       info.user_id = group.user_id
