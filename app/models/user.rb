5604c45f9dfa4dc2cdb3f4daa338d7eb87effc48
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2019-10-21 17:00:56 +0900

    sad

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
 
-  has_one_attached :avater
+  #has_one_attached :avater
 
   def following?(other_user)
     following_relationships.find_by(following_id: other_user.id)
