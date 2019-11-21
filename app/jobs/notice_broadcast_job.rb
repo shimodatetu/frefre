3e776ac05e4d11e05d71451cee3733c0912fc9c7
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2019-11-21 17:11:53 +0900

    asd

--- app/jobs/notice_broadcast_job.rb
+++ app/jobs/notice_broadcast_job.rb
@@ -2,8 +2,7 @@ class NoticeBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast 'notice_channel',notice_id: message.notice_id,
-    message: render_message(message),users[message.users.first,message.users.last]
+    ActionCable.server.broadcast 'notice_channel',notice_id: message.notice_id,message: render_message(message),users[message.users.first,message.users.last]
   end
 
   private
