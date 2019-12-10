4c78acb6b7b6ada523a634951e7a624c9b6ef5c0
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2019-12-10 19:05:15 +0900

    asd

--- db/migrate/20191125063341_remove_delete_from_reportuser.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeleteFromReportuser < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :reportusers, :delete, :boolean
-  end
-end
--- db/migrate/20191125063349_remove_delete_from_reportpost.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDeleteFromReportpost < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :reportposts, :delete, :boolean
-  end
-end
