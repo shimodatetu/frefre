e624046f685df1ee600fb35ca9c71ac2ad02f57a
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2020-02-06 21:44:46 +0900

    asd

--- test/controllers/other_profile_controller_test.rb
+++ test/controllers/other_profile_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class OtherProfileControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get other_profile_show_url
     assert_response :success
   end
 
