47019237e7f2039eae25248412c645ce8f3e3097
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2020-03-19 01:19:09 +0900

    asd

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,10 @@ Bundler.require(*Rails.groups)
 module Webapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.action_view.automatically_disable_submit_tag = false
     config.load_defaults 5.2
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-    config.middleware.use Rack::Attack
-    config.autoload_paths += Dir["#{config.root}/lib"]
     # config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
