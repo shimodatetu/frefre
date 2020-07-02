7486b843ad9c19046b6c4c14847e3fd653d760ff
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2020-07-02 17:52:03 +0900

    asd

--- app/controllers/other_profile_controller.rb
+++ app/controllers/other_profile_controller.rb
@@ -4,14 +4,14 @@ class OtherProfileController < ApplicationController
     if User.find_by(id:params[:id].to_i)
       other_user = User.find_by(id:params[:id].to_i)
     end
-    if params[:id] == nil || params[:id] == "1" || params[:id] == "2" || params[:id] == "3" || params[:id] == "4"
+    if params[:type] == nil || params[:type] == "1" || params[:type] == "2" || params[:type] == "3" || params[:type] == "4"
       page_id = params[:page].to_i
       if page_id.nil? || page_id.to_i < 1
         page_id = 1
       end
       per = 10
       other_user_id = other_user.id
-      if params[:type] == nil || params[:id] == "1"
+      if params[:type] == nil || params[:type] == "1"
         @join_groups = other_user.threadtypes.all.page(page_id).per(per)
         @make_groups = Threadtype.all.where(leader_id:other_user_id).page(1).per(per)
         @make_threads = other_user.groups.all.page(1).per(per)
