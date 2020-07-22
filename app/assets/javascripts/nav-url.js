7a6f40b6b0b36ca66c82cb1e522995e6b1c7e4ee
霜田 哲之介 <tetuprogram@yahoo.co.jp>
2020-07-22 16:16:13 +0900

    asd

--- app/assets/javascripts/nav-url.js
+++
@@ -1,7 +0,0 @@
-hash = window.location.hash
-  hash and $("ul.nav a[href=\"" + hash + "\"]").tab("show")
-  $(".nav-tabs a").click (e) ->
-    $(this).tab "show"
-    scrollmem = $("body").scrollTop()
-    window.location.hash = @hash
-    $("html,body").scrollTop scrollmem
